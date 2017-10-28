d0246b06249ee44f694cca4466d6a031b9662efa
Max Isom <codetheweb@icloud.com>
2017-10-28 15:49:21 -0500

    Inital commit

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# homebridge-yamaha-scene
-Turns on and off a Yamaha AV receiver and set it to a scene
+homebridge-yamaha-scene
+========================
+
+Turns on and off a Yamaha AV receiver and sets it to a default scene and volume.
+Used to automate our home entertainment setup.
+
+# Installation
+`npm install -g homebridge-yamaha-scene`
+
+# Example config.json
+    {
+      "accessory": "YamahaScene",
+      "name": "Yamaha AV",
+      "ip": "xxx.yyy.0.zzz",
+      "scene": 1,
+      "volume": -20
+    }
---
+++ index.js
@@ -0,0 +1,49 @@
+const YamahaAPI = require("yamaha-nodejs");
+
+module.exports = function(homebridge) {
+  Service = homebridge.hap.Service;
+  Characteristic = homebridge.hap.Characteristic;
+
+  homebridge.registerAccessory("homebridge-yamaha-scene", "YamahaScene", YamahaScene);
+}
+
+function YamahaScene(log, config) {
+  this.log = log;
+  this.name = config.name;
+  this.volume = config.volume || -30;
+  this.initalScene = config.inital_scene || 1;
+  this.yamaha = new YamahaAPI(config.ip);
+
+  this._service = new Service.Switch(this.name);
+  this._service.getCharacteristic(Characteristic.On).on('set', this._set.bind(this));
+  this._service.getCharacteristic(Characteristic.On).on('get', this._get.bind(this));
+}
+
+YamahaScene.prototype.getServices = function() {
+  return [this._service];
+}
+
+YamahaScene.prototype._get = function(callback) {
+  this.yamaha.isOn().then(function(result) {
+    callback(null, result);
+  });
+}
+
+YamahaScene.prototype._set = function(on, callback) {
+  if (on) {
+    var that = this;
+    this.yamaha.powerOn().then(function() {
+      that.yamaha.SendXMLToReceiver('<YAMAHA_AV cmd="PUT"><Main_Zone><Scene><Scene_Load>Scene ' + that.initalScene + '</Scene_Load></Scene></Main_Zone></YAMAHA_AV>').then(function() {
+        that.yamaha.setVolumeTo(that.volume * 10, 'Main_Zone').then(function() {
+          callback(null, true);
+        });
+      });
+
+    });
+  }
+  else {
+    this.yamaha.powerOff().then(function() {
+      callback(null, false);
+    });
+  }
+}
